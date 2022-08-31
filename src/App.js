6177e597d79b7e683c2dcc46ba2aea8e2a24e001
cadrec <ccarriga@chapman.edu>
2022-08-31 15:49:32 -0400

    Tier 2 Edits

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCampuses } from "./store/campusReducer";
 import { getStudents } from "./store/studentReducer";
 import { Routes, Route, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { Campuses } from "./components/campuses";
+import { Students } from "./components/students";
 
 
 function App(){
     const dispatch = useDispatch();
     const state = useSelector((state) => state);
 
+    
+
     return(
         <>
-            <div></div>
+            <div>
+                <Link to={"/campuses"}>
+                    <button>Campuses ({campuses.length})</button>
+                </Link>
+                
+                <Link to={"/students"}>
+                    <button>Students ({students.length})</button>
+                </Link>
+            </div>
+            <Routes>
+                <Route path="campuses" element={<Campuses />} />
+                <Route path="students" element={<Students />} />
+            </Routes>
         </>
 
     )
